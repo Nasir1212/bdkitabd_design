4b9195807840bd5d3e9b4bf54ba734de2bd0750d
Nasir@1212 <nnasiruddin1996@gmail.com>
2023-03-29 12:52:50 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            
+        },
+         400:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
